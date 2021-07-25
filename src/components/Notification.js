3cd41360a82564716100dc1044f5b7d9e381609f
Pannu786 <thepannu786@gmail.com>
2021-07-25 23:15:33 +0530

    dne

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Notification = () => {
+const Notification = ({showNotification}) => {
   return (
     <div className={`notification-container ${showNotification ? 'show' : ''}`}>
       <p>You have already entered this letter</p>
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { checkWin } from '../helpers/helpers';
 
-const Popup = () => {
+const Popup = ({
+  correctLetters,
+  wrongLetters,
+  selectedWord,
+  setPlayable,
+  playAgain,
+}) => {
+  let finalMessage = '';
+  let finalMessageRevealWord = '';
+  let playable = true;
+
+  if (checkWin(correctLetters, wrongLetters, selectedWord) === 'win') {
+    finalMessage = 'Congratulations! You won! 😃';
+    playable = false;
+  } else if (checkWin(correctLetters, wrongLetters, selectedWord) === 'lose') {
+    finalMessage = 'Unfortunately you lost. 😕';
+    finalMessageRevealWord = `...the word was: ${selectedWord}`;
+    playable = false;
+  }
+
+  useEffect(() => {
+    setPlayable(playable);
+  });
+
   return (
-    <div className='popup-container' id='popup-container'>
+    <div
+      className='popup-container'
+      style={finalMessage !== '' ? { display: 'flex' } : {}}
+    >
       <div className='popup'>
         <h2>{finalMessage}</h2>
         <h3>{finalMessageRevealWord}</h3>
